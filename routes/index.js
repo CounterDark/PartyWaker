2926cd6564b63c7292a0f78aac9895df2ce406bd
CounterDark <aleksanderkunkowski@gmail.com>
2024-01-12 15:00:09 +0100

    removed unused http

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-var HTTP = process.env.HTTP || 'http://localhost:3000';
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'PartyWaker', css: '/stylesheets/style.css', script: '/javascripts/script.js', buttonUrl: HTTP + '/menu' });
+  res.render('index', { title: 'PartyWaker', css: '/stylesheets/style.css', script: '/javascripts/script.js'});
 });
 
 module.exports = router;
