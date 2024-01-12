389253a1e28e116d5bc8107464f5a68def288285
CounterDark <aleksanderkunkowski@gmail.com>
2024-01-12 14:50:46 +0100

    quick fix for server

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var HTTP = process.env.HTTP || 'http://localhost:3000';
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'PartyWaker', css: HTTP + '/stylesheets/style.css', script: HTTP + '/javascripts/script.js', buttonUrl: HTTP + '/menu' });
+  res.render('index', { title: 'PartyWaker', css: '/stylesheets/style.css', script: '/javascripts/script.js', buttonUrl: HTTP + '/menu' });
 });
 
 module.exports = router;
